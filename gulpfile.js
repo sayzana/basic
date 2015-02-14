f5e9c4e3619a31a6dfe3a9565a7fe5c533117bc0
Sayzana Saryglar <whitechaos91@mail.ru>
2015-02-14 10:17:44 +0300

    add some packages

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@
 
 var gulp = require("gulp"),
 	connect = require("gulp-connect"),
-	opn = require("opn");
+	opn = require("opn"),
+	wiredep = require('wiredep').stream,
+    clean = require('gulp-clean'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    minifyCss = require('gulp-minify-css');
 
 gulp.task('connect', function() {
   connect.server({
@@ -27,10 +33,35 @@ gulp.task('js', function() {
     .pipe(connect.reload());
 });
 
+gulp.task('wiredep', function () {
+  gulp.src('./app/*.html')
+    .pipe(wiredep({
+      directory: 'app/bower_components'
+    }))
+    .pipe(gulp.dest('./dest'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./app/*.html', ['html']);
   gulp.watch('./app/css/*.html', ['css']);
   gulp.watch('./app/js/*.html', ['js']);
+  gulp.watch('bower.json', ['wiredep']);
 });
 
+gulp.task('html', function () {
+    var assets = useref.assets();
+    
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', function () {
+    return gulp.src('dist', {read: false})
+        .pipe(clean());
+});
 gulp.task('default', ['connect', 'watch']);
